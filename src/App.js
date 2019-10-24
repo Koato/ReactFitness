04ac5a8b52f354ff7db0356c13b30a96eb8025f1
Andy Gomez <i201622650@cibertec.edu.pe>
2019-10-24 16:19:04 -0500

    editanto los valores iniciales de React

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Editar <code>src/App.js</code> y guarda para recargar.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Leer sobre React
         </a>
       </header>
     </div>
