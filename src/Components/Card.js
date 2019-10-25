0d3879f57b88656caaab319673e429c1d25cfe0c
Andy Gomez <i201622650@cibertec.edu.pe>
2019-10-24 20:18:38 -0500

    uso de props

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,24 +1,24 @@
 import React from "react";
-import exercise from "../images/exercise.png";
+import exerciseIMG from "../images/exercise.png";
+import circlesIMG from "../images/circles.png";
 import './styles/Card.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 class Card extends React.Component {
     render() {
+        const { title, description, img, leftColor, rightColor } = this.props;
         return (
-            <div className="card mx-auto Fitness-Card">
+            <div className="card mx-auto Fitness-Card" style={{
+                backgroundImage: `url({${circlesIMG}}), linear-gradient(to right, ${leftColor}, ${rightColor})`
+            }}>
                 <div className="card-body">
                     <div className="row center">
                         <div className="col-6">
-                            <img src={exercise} className="float-right" />
+                            <img src={img} className="float-right" />
                         </div>
                         <div className="col-6 Fitness-Card-Info">
-                            <h1>
-                                Guía técnica
-                            </h1>
-                            <p>
-                                Lectura de actividades y rutinas
-                            </p>
+                            <h1>{title}</h1>
+                            <p>{description}</p>
                         </div>
                     </div>
                 </div>
--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@ import Card from './Components/Card';
 
 // ReactDOM.render(Que, Donde);
 // ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Card />, document.getElementById('root'));
+ReactDOM.render(<Card title="Guía técnica"
+    description="Lectura de actividades y rutinas"
+    img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
+    leftColor="#A74CF2"
+    rightColor="#617BFB" />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
