253e3305c9e8aeaf2d278a1bdd0040dd0384b2bb
Andy Gomez <i201622650@cibertec.edu.pe>
2019-10-28 16:01:31 -0500

    mejorando UX

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,5 +1,6 @@
 import React from "react";
 import circlesIMG from "../images/circles.png";
+import emptyIMG from "../images/empty.png";
 import './styles/Card.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -29,12 +30,12 @@ class Card extends React.Component {
         return (
             // solo retorna un solo valor
             <div className="card mx-auto Fitness-Card" style={{
-                backgroundImage: `url({${circlesIMG}}), linear-gradient(to right, ${leftColor}, ${rightColor})`
+                backgroundImage: `url(${circlesIMG}), linear-gradient(to right, ${leftColor || '#56CCF2'}, ${rightColor || '#2F80ED'})`
             }}>
                 <div className="card-body">
                     <div className="row center">
                         <div className="col-6">
-                            <img src={img} className="float-right" />
+                            <img src={img || emptyIMG} className="float-right" />
                         </div>
                         <div className="col-6 Fitness-Card-Info">
                             <h1>{title}</h1>
--- src/Components/pages/ExcercisesNew.js
+++ src/Components/pages/ExcercisesNew.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ExerciseForm from "../ExerciseForm";
 import Card from "../Card";
+import "../styles/ExerciseNew.css";
 
 class ExcercisesNew extends React.Component {
 
@@ -25,11 +26,11 @@ class ExcercisesNew extends React.Component {
 
     render() {
         return (
-            <div className="row">
-                <div className="col-sm">
+            <div className="ExerciseNew_Lateral_Spaces row">
+                <div className="col-sm ExerciseNew_Card_Space">
                     <Card {...this.state.form} />
                 </div>
-                <div className="col-sm">
+                <div className="col-sm ExerciseNew_Card_Space">
                     <ExerciseForm onChange={this.handleCharge} form={this.state.form} />
                 </div>
             </div>
