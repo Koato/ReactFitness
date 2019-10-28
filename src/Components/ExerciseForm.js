f92a9fb601fd4746a8c89e1c25a0f541104ca6a2
Andy Gomez <i201622650@cibertec.edu.pe>
2019-10-28 13:33:55 -0500

    uso de componentes padres - Lifting state up

---
+++ src/Components/ExerciseForm.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+class ExerciseForm extends React.Component {
+
+    handleSubmit = e => {
+        e.preventDefault();
+        console.log(this.state);
+    }
+
+    render() {
+        const { onChange, form } = this.props;
+        return (
+            <div className="container">
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <input type="text" placeholder="Título" className="form-control" name="title" onChange={onChange} value={form.titulo} />
+                    </div>
+                    <div className="form-group">
+                        <input type="text" placeholder="Descripción" className="form-control" name="description" onChange={onChange} value={form.descripcion} />
+                    </div>
+                    <div className="form-group">
+                        <input type="text" placeholder="Imagen" className="form-control" name="img" onChange={onChange} value={form.imagen} />
+                    </div>
+                    <div className="form-row">
+                        <div className="col">
+                            <input type="text" placeholder="Color izquierdo" className="form-control" name="leftColor" onChange={onChange} value={form.izquierda} />
+                        </div>
+                        <div className="col">
+                            <input type="text" placeholder="Color derecho" className="form-control" name="rightColor" onChange={onChange} value={form.derecha} />
+                        </div>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Enviar</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default ExerciseForm;
--- src/Components/pages/ExcercisesNew.js
+++ src/Components/pages/ExcercisesNew.js
@@ -1,10 +1,37 @@
 import React from "react";
+import ExerciseForm from "../ExerciseForm";
+import Card from "../Card";
 
 class ExcercisesNew extends React.Component {
+
+    state = {
+        form: {
+            title: '',
+            description: '',
+            img: '',
+            leftColor: '',
+            rightColor: ''
+        }
+    }
+
+    handleCharge = e => {
+        this.setState({
+            form: {
+                ...this.state.form,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+
     render() {
         return (
-            <div>
-                ExcercisesNew.js
+            <div className="row">
+                <div className="col-sm">
+                    <Card {...this.state.form} />
+                </div>
+                <div className="col-sm">
+                    <ExerciseForm onChange={this.handleCharge} form={this.state.form} />
+                </div>
             </div>
         );
     }
